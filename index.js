72dc8ad154c5bab86428ffc4ddfd0db5592a3444
Kevin Clark <kevin@thisbailiwick.com>
2023-09-11 22:16:56 -0400

    miscellaneous changes;
    move processing code to src directory;
    change to module importing/exporting;
    make index import new file in src directory;
    add jsdocs;

--- index.js
+++ index.js
@@ -1,74 +1,12 @@
-let fs = null;
-let createCsvWriter = null;
-let filePath = null;
-let fileName = null;
+// import {runProcessing} from "./src/process-har.js";
+const filePath = process.argv[3];
+const fileTypesToProcess = process.argv[4] !== 'null' && typeof process.argv[4] !== 'undefined' ? process.argv[4].split(',') : [];
+const resourceTypesToProcess = process.argv[5] !== 'null' && typeof process.argv[5] !== 'undefined' ? process.argv[5].split(',') : [];
+// runProcessing(filePath, fileTypesToProcess, resourceTypesToProcess);
 
-const readFile = (filePath) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filePath, 'utf8', (err, data) => {
-			if (err) {
-				console.error('An error occurred while reading the HAR file.');
-				reject(err);
-			}
-			resolve(JSON.parse(data));
-		});
-	});
-};
 
-const getInitiatorStack = (entry) => {
-	if (entry._initiator.type === 'script' || entry._initiator.type === 'parser') {
-		return entry._initiator.stack
-			? entry._initiator.stack.callFrames.map(
-				(callFrame) => `${callFrame.url} line ${callFrame.lineNumber} column ${callFrame.columnNumber}`
-			).join('\n')
-			: `${entry._initiator.url} line ${entry._initiator.lineNumber}`;
-	}
-};
+import HarProcessor from './src/ProcessHar.js';
 
-const getJsFiles = (entries) => {
-	return entries
-		.filter((entry) => entry.request.url.endsWith('.js') || entry.request.url.endsWith('.mjs'))
-		.map((entry) => ({
-			url: entry.request.url,
-			initiatorStack: getInitiatorStack(entry),
-			wait: entry.timings.wait,
-			downloading: entry.timings.receive,
-			totalWaitReceive: entry.timings.wait + entry.timings.receive,
-			total: entry.time,
-		}));
-};
+const harProcessor = new HarProcessor();
+harProcessor.runProcessing(filePath, fileTypesToProcess, resourceTypesToProcess);
 
-const writeCsvFile = (filename, records) => {
-	const csvWriter = createCsvWriter({
-		path: filename + '.csv',
-		header: [
-			{ id: 'url', title: 'URL' },
-			{ id: 'initiatorStack', title: 'Initiator Stack' },
-			{ id: 'wait', title: 'Wait for server response' },
-			{ id: 'downloading', title: 'Downloading from server' },
-			{ id: 'totalWaitReceive', title: 'Total Wait and Downloading' },
-			{ id: 'total', title: 'Total' },
-		],
-	});
-	return csvWriter.writeRecords(records);
-};
-
-module.exports = {
-    readFile,
-    getJsFiles,
-    writeCsvFile
-}
-
-// This condition checks to see if this script is the main module (i.e., not being imported)
-if (require.main === module) {
-	fs = require('fs');
-	createCsvWriter = require('csv-writer').createObjectCsvWriter;
-	filePath = process.argv[3];
-	fileName = filePath.split('/').pop();
-
-	readFile(filePath)
-	.then((harData) => getJsFiles(harData.log.entries))
-	.then((jsFiles) => writeCsvFile(fileName, jsFiles))
-	.then(() => console.log('...Done'))
-	.catch((error) => console.log(error));
-}
